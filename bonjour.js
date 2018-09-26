d491e38aacb08039059f70e525deb68e215b7fdb
Jango <arthur.vouloir@gmail.com>
2018-09-26 13:10:09 +0200

    Confirm 18

--- bonjour.js
+++ bonjour.js
@@ -1,1 +1,9 @@
 alert('Bienvenue sur mon site!');
+
+if (confirm('Pour accéder à ce site vous devez avoir 18 ans ou plus, cliquez sur "OK" si c\'est le cas.')) {
+    alert('Vous allez être redirigé vers le site.');
+}
+
+else {
+    alert("Désolé, vous n'avez pas accès à ce site.");
+}
